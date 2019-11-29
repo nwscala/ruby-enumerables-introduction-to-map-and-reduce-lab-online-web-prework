2494768a6559f8836a05c04c78aa323ea63b3379
Noah Scala <tobi8912@gmail.com>
2019-11-29 21:24:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,10 @@
 # My Code here....
 def map_to_negative(source_array)
-  
+  index = 0
+  new_array = []
+  while index < source_array.length do 
+    new_array[index] = source_array[index] * -1
+    index += 1 
+  end 
+  new_array
 end 
