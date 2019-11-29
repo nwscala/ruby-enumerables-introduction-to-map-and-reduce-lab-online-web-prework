c0693ce0f698cabafb799343a8b0ea2db869c914
Noah Scala <tobi8912@gmail.com>
2019-11-29 21:26:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,7 +7,28 @@ def map_to_negativize(source_array)
     index += 1 
   end 
   new_array
-end 
+end
+
 def map_to_no_change(source_array)
   source_array
+end
+
+def map_to_double(source_array)
+  index = 0
+  new_array = []
+  while index < source_array.length do 
+    new_array[index] = source_array[index] * 2
+    index += 1 
+  end 
+  new_array
+end 
+
+def map_to_square(source_array)
+  index = 0
+  new_array = []
+  while index < source_array.length do 
+    new_array[index] = source_array[index] ** 2
+    index += 1 
+  end 
+  new_array
 end 
